07051b9966c735602bda8c03f32ea0e03955e754
ggs134 <inditow@gmail.com>
2015-10-02 07:08:12 +0900

    schema변경

--- csvimport.py
+++ csvimport.py
@@ -3,8 +3,12 @@
 import csv
 from pymongo import MongoClient
 
+#Maro
 client = MongoClient('103.49.44.44',27017)
+#local
 #client = MongoClient()
+#Hwi
+#client = MongoClient('52.69.236.246',27017)
 db = client.snack
 postsCollection = db.posts
 mainCollection = db.main
@@ -16,9 +20,9 @@ def main():
     with open('nongsim.csv','r') as csvFile:
         reader = csv.reader(csvFile)
         for row in csv.reader(csvFile):
-            nut_cont = listToNutDic(deliToList(row[2]))
+            nut_cont = listToIngDic(deliToList(row[2]))
             #data = {"snack_name" :row[1].decode('utf-8').encode('utf-8')}
-            postsData={"snack_name" :row[1],"manufacturer":"농심","ingredient":nut_cont, "avr_grade":None, "comments": None}
+            postsData={"snack_name" :row[1],"manufacturer":"농심","nutrition": nut_cont,"ingredient":makeNutrition(row), "avr_grade":None, "comments": []}
             mainData={"manufacturer":"농심","snack_name":row[1], "avr_grade":None}
             userData = {"email": None, "password": None, "phone_number": None, "intruduction" : None, "nickname": None, "join_date":None, "Comments":None}
             commentsData = {"by":{"id": None, "nickname": None}, "text": None, "grade": None, "c_timestamp": None, "snack_name": None }
@@ -40,9 +44,24 @@ def deliToList(deliStr):
     a=deliStr.split(',')
     return [value for value in a]
 
-def listToNutDic(lists):
-    return [{"ing_name":value , "ing_amount": None }for value in lists]
+def listToIngDic(lists):
+    return [{"ing_name":value , "ing_amount": None , "ing_recommanded": None}for value in lists]
 
+def makeNutrition(lst):
+    lists=[]
+
+    lists.append({"열량)": lst[6]})
+    lists.append({ "탄수화물": lst[7]})
+    lists.append({"단백질": lst[8]})
+    lists.append({"지방": lst[9]})
+    lists.append({"당류" :lst[10]})
+    lists.append({ "나트륨" : lst[11]})
+    lists.append({"콜레스테롤" : lst[12]})
+    lists.append({"포화지방산" :lst[13]})
+    lists.append({"트렌스지방산" :lst[14]})
+    lists.append({ "칼슙" :lst[15] })
+
+    return lists
 if __name__=="__main__":
     client.drop_database("snack")		
     main()
